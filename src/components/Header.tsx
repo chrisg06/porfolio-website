fa911b1883e5f7a12b91065fb31748447a6e75e5
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2024-08-26 23:24:08 +0800

    update header to use links to sections

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 const navigation = [
-    {page: 'Home', path: '/'},
-    {page: 'About', path: '/about'},
-    {page: 'Projects', path: '/projects'},
-    {page: 'Contact', path: '/contact'},
+    {page: 'About', id: '#about'},
+    {page: 'Skills', id: '#skills'},
+    {page: 'Projects', id: '#projects'},
+    {page: 'Contact', id: '#contact'},
 ];
 
 
@@ -12,8 +14,8 @@ export default function Header() {
             <nav>
                 <ul className="flex items-center justify-center">
                     {navigation.map((navItem) => (
-                        <li key={navItem.path} className="py-4 px-8 hover:scale-125 duration-100">
-                            <a href={navItem.path} className="hover:text-sky-600">{navItem.page}</a>
+                        <li key={navItem.id} className="py-4 px-8 hover:scale-125 duration-100">
+                            <Link href={navItem.id} scroll={true} className="hover:text-sky-600">{navItem.page}</Link>
                         </li>
                     ))}
                 </ul>
