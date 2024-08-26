8122692ecbbf594086ae1a6c83e3abc1ffc9f50a
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2024-08-26 23:25:18 +0800

    scroll to top button + smooth scroll

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import HeroTitle from "@/components/heroTitle";
 
 export default function ContactPage() {
   return (
-    <section id="projects" className="min-h-screen">
+    <section id="contact" className="min-h-screen">
       
     <HeroTitle title="Contact" />
     </section>
---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+
+export default function ScrollToTop() {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        history.replaceState(null, '', window.location.pathname);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className="fixed bottom-4 right-4 p-4 bg-sky-500 text-white rounded-full shadow-lg hover:bg-sky-600"
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        history.replaceState(null, '', window.location.pathname);
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 10l7-7m0 0l7 7m-7-7v18"
+        />
+      </svg>
+      <a href="#" className="sr-only"></a>
+    </button>
+  );
+}
