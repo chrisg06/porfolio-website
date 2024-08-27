1fa9bdd0da876d197f1c0835baf0fa5c8c8ed0a3
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2024-08-27 22:47:23 +0800

    make underline optional on heroTitle

--- src/components/heroTitle.tsx
+++ src/components/heroTitle.tsx
@@ -1,14 +1,17 @@
 interface HeroTitleProps {
-    title: string;
-};
+  title: string;
+  underline?: boolean;
+}
 
-export default function HeroTitle({ title }: HeroTitleProps) {
-    return (
-        <>
-        <h1 className="pt-4 bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold tracking-tighter text-transparent">
-          {title}
-        </h1>
+export default function HeroTitle({ title, underline = true }: HeroTitleProps) {
+  return (
+    <>
+      <h1 className="pt-4 bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold tracking-tighter text-transparent">
+        {title}
+      </h1>
+      {underline && (
         <hr className="mx-auto my-4 h-1 w-60 border-0 bg-gray-100" />
-        </>
-    );
+      )}
+    </>
+  );
 }
