e470eac6746cb2cd041d54a3180d491f9e7a75d7
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2024-08-28 23:17:28 +0800

    fix contact form endpoint

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -4,13 +4,14 @@ import Mailgun from "mailgun.js";
 
 const API_KEY = process.env.MAILGUN_API_KEY || "";
 const DOMAIN = process.env.MAILGUN_DOMAIN || "";
-const EMAIL_RECIPIENT = process.env.EMAIL_RECEPIENT || "";
+const EMAIL_RECIPIENT = process.env.EMAIL_RECIPIENT || "";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { name, email, message } = req.body;
+  const { firstName, lastName, email, message } = req.body;
+  const fullName = `${firstName} ${lastName}`.trim();
 
   const mailgun = new Mailgun(FormData);
   const client = mailgun.client({
@@ -19,18 +20,18 @@ export default async function handler(
   });
 
   const messageData = {
-    from: "No Reply <noreply@mg.chrisgardiner.org>",
+    from: `${fullName} <contact@mg.chrisgardiner.org>`,
     to: EMAIL_RECIPIENT,
     subject: "New message from Chris Gardiner's Portfolio",
     text: `Hello,
     
-    You have a new message from ${name} (${email}):
+    You have a new message from ${fullName} (${email}):
     
     ${message}`,
   };
 
   try {
-    const emailRes = await client.messages.create(DOMAIN, messageData);
+    await client.messages.create(DOMAIN, messageData);
   } catch (err: any) {
     console.error("Error seding email", err);
   }
