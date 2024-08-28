1e15e00e30cae288371cac95b9e9d3604e409bc9
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2024-08-28 23:05:41 +0800

    update projects section photos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -84,14 +84,14 @@ const data = [
 
         <div className="grid grid-cols-2 gap-4">
           <Image
-            src="/img/projects/vatpac-core.png"
+            src="/img/projects/vatpac-core-1.png"
             alt="startup template"
             width={800}
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
           <Image
-            src="/img/projects/pilot-assist-api-2.png"
+            src="/img/projects/vatpac-core-2.png"
             alt="startup template"
             width={800}
             height={500}
